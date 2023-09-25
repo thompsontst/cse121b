cd1c054d70ad05dfb499e2089c637830ed149612
thompsontst <thompsonabove@gmail.com>
2023-09-25 15:33:57 +0100

    week02 work

---
+++ javascript-demo/script/demo.js
@@ -0,0 +1,6 @@
+alert('hello');
+2 + 2
+alert('Good job!');
+var date = document.lastModified;
+                document.getElementById("lastModified").innerHTML = "Last Date Modified : " + date;
+                
---
+++ scripts/w02-task.js
@@ -0,0 +1,54 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+
+let fullName = "Aniekan Thompson";
+
+let currentYear = new Date();
+
+const profilePicture = "images/profilePics.jpg";
+
+
+
+/* Step 3 - Element Variables */
+
+
+const nameElement = document.getElementById("name");
+
+const foodElement = document.getElementById("food");
+
+const partnerElement = document.getElementById("partner");
+
+const yearElement =  document.querySelector("#year");
+
+const imageElement =  document.getElementById("img[src =images/profilePicture.jpg]");
+
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+//imageElement.setAttribute('src', profilePicture);
+//imageElement.setAttribute('alt', 'profile picture of [fullName]');
+yearElement.textContent = `${currentYear}`;
+/* Step 5 - Array */
+
+let  favoriteFoods = ['Plantain Porridge','jollof rice', 'Afang soup',' Banana Glace', 'rice peppersoup','chicken peripperi'];
+foodElement.textContent = favoriteFoods;
+const anotherfavfood = 'Porridge Beans';
+favoriteFoods.push(anotherfavfood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+
+
+
+
+
